ad63ba0f78d26919982e040d1a02a774d2e58ff9
Jason_NEWTON_ALBERT <126969936+JasonAlbertNewton@users.noreply.github.com>
2023-05-13 15:03:22 +0800

    Add files via upload

--- Algo_trade/avg_mov_cro.py
+++ Algo_trade/avg_mov_cro.py
@@ -1,2 +1,26 @@
-def avg_mov_cro(mov_1 , mov_2):
-	pass
+import pandas as pd
+import numpy as np
+
+#this function return at that period of time of back testing,  
+class average_moving_cross:	
+	def __init__(self , data , mov_1 , mov_2):
+		self.data = data
+		self.mov_1 = mov_1
+		self.mov_2 = mov_2
+		
+	#this function is to calcualte the date range of the data
+	def gt_range_data(self):
+		print(self.data.head(self.mov_1))	
+
+	def avg_mov_cro(self):
+		Close_level = data['Close']
+	
+		mov_1 = int(self.mov_1)
+		mov_2 = int(self.mov_2)
+
+		if(Close_level.size < self.mov_1 or Close_level.size < self.mov_2):
+			return []
+
+
+
+	
--- Algo_trade/main.py
+++ Algo_trade/main.py
@@ -1,23 +1,43 @@
 import yfinance as yf
+import numpy as np
+import pandas as pd
 import os 
+
+from avg_mov_cro import average_moving_cross
 
 os.system('clear')
 
 while (1):
-#import datas
-	stock_check = input("INPUT THE STOCK YOU WANT TO CHECK: ")
+#import data
+	stock_check = input("INPUT THE STOCK YOU WANT TO CHECK: [-1 to quit] ")
+	
+	if(stock_check == "-1"):
+		break
+	
 	start_date  = input("The begining of the data you want: [YYYY-MM-DD] ")
 	end_date = input("The end of the date you want: [YYYY-MM-DD] ")
 
 	data = yf.download(stock_check,start=start_date , end=end_date)
-	
-	user_input = int(input("\nINPUT OPTION: \n 1. Read data \n 2. Analysis Data \n"))
-	
-	if(user_input == 1):
-		print(data.tail())
-	if(user_input == 2):
-		print(data[0])
-	
-	if(user_input == -1):
-		break
+	while(1):
+		user_input = int(input("\nINPUT OPTION: \n 1. Read data \n 2. Specific Column \n"))
+		
+		if(user_input == 1):
+			print(data.tail())
+		if(user_input == 2):
+			arr = []
+			while(1):
+				user_specific_column = input("\nOpen , Close , High , Low , Volume : one time input one , finsih input -1\n")
+				if(user_specific_column == "-1"):
+					break
+				else:
+					arr.append(user_specific_column)
+			temp = data[arr]
+			print(temp)
+		if(user_input == 3):
+			avg_mov_cro= average_moving_cross(data , 10 , 20)			
+			avg_mov_cro.gt_range_data()
+
+		
+		if(user_input == -1):
+			break
 
