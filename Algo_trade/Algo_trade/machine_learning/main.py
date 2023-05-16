3888bc411e331ffe72b10a4a0972a443ab6b1901
Jason_NEWTON_ALBERT <126969936+JasonAlbertNewton@users.noreply.github.com>
2023-05-17 01:20:25 +0800

    Delete Algo_trade directory

--- Algo_trade/Algo_trade/machine_learning/main.py
+++
@@ -1,8 +0,0 @@
-import torch 
-import torchvision
-import torch.nn as nn
-import numpy as np
-import torchvision.transforms as transforms
-
-
-x = torch.tensor(1, require
--- Algo_trade/lib/test.py
+++
@@ -1,2 +0,0 @@
-#back testing
-import pandas as pd
